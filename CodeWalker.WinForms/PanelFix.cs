19fad3f44b4f4731d135ec218c0e86645b4f6e93
FiftyShadesOfBlue <Skytut16@gmail.com>
2018-03-16 17:12:04 -0400

    Added Zooming And Panning in YtdForm

---
+++ CodeWalker.WinForms/PanelFix.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeWalker.WinForms
+{
+    public partial class PanelFix : Panel
+    {
+        public PanelFix()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            
+        }
+    }
+}
