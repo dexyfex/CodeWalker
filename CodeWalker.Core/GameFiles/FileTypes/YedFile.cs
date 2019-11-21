0f7f35d28b3d3d0d266159c521c5daca3b6a62fa
dexy <dexyfex@outlook.com>
2019-11-22 00:34:27 +1100

    Added RPF Explorer .yed, .yfd, .yld generic support

---
+++ CodeWalker.Core/GameFiles/FileTypes/YedFile.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWalker.GameFiles
+{
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public class YedFile : GameFile, PackedFile
+    {
+        public ExpressionDictionary ExpressionDictionary { get; set; }
+
+        public string LoadException { get; set; }
+
+
+        public YedFile() : base(null, GameFileType.Yed)
+        {
+        }
+        public YedFile(RpfFileEntry entry) : base(entry, GameFileType.Yed)
+        {
+        }
+
+        public void Load(byte[] data, RpfFileEntry entry)
+        {
+            Name = entry.Name;
+            RpfFileEntry = entry;
+            //Hash = entry.ShortNameHash;
+
+
+            RpfResourceFileEntry resentry = entry as RpfResourceFileEntry;
+            if (resentry == null)
+            {
+                throw new Exception("File entry wasn't a resource! (is it binary data?)");
+            }
+
+            ResourceDataReader rd = null;
+            try
+            {
+                rd = new ResourceDataReader(resentry, data);
+            }
+            catch (Exception ex)
+            {
+                //data = entry.File.DecompressBytes(data); //??
+                LoadException = ex.ToString();
+            }
+
+            ExpressionDictionary = rd?.ReadBlock<ExpressionDictionary>();
+
+        }
+    }
+}
