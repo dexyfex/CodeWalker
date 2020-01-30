38ffd310fb2e7f2794af07ebdc3ab109768de348
Liam Fisher <liamfisher1@gmail.com>
2020-01-30 19:14:46 +0000

    Removed unused directives

--- CodeWalker.Core/GameFiles/FileTypes/YndFile.cs
+++ CodeWalker.Core/GameFiles/FileTypes/YndFile.cs
@@ -2,11 +2,7 @@ using SharpDX;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 
 namespace CodeWalker.GameFiles
