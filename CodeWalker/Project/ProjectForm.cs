37329a2eaf67fea45fe2bee175757f9c2acb9490
dexy <dexyfex@outlook.com>
2021-11-21 05:02:30 +1100

    Attempt to refocus WorldForm when selecting objects with ProjectForm open - ProjectForm is/was stealing focus due to DockPanel issue

--- CodeWalker/Project/ProjectForm.cs
+++ CodeWalker/Project/ProjectForm.cs
@@ -7680,6 +7680,10 @@ namespace CodeWalker.Project
                         ShowCurrentProjectItem(false);
                         ShowProjectItemInProcess = false;
                     }
+                    if (WorldForm != null)
+                    {
+                        WorldForm.Focus();//sometimes newly created panel steals focus from the WorldForm, this is a hack to give it back
+                    }
                 }
             }
             catch { }
