92bd3706d131d092847393352341043575d805d4
ook3d <47336113+ook3D@users.noreply.github.com>
2025-04-07 13:36:50 -0400

    Improve Error Report Tool
    Format the error log to be easier to read, add a copy to clipboard and save log file button

--- CodeWalker.ErrorReport/ReportForm.Designer.cs
+++ CodeWalker.ErrorReport/ReportForm.Designer.cs
@@ -30,36 +30,61 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReportForm));
             this.label1 = new System.Windows.Forms.Label();
-            this.ErrorTextBox = new System.Windows.Forms.TextBox();
+            this.ErrorTextBox = new System.Windows.Forms.RichTextBox();
+            this.SaveLogButton = new System.Windows.Forms.Button();
+            this.CopyToClipboardButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Kozuka Gothic Pro R", 9F);
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.Size = new System.Drawing.Size(125, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Last CodeWalker.exe error:";
+            this.label1.Text = "Last CodeWalker Error:";
             // 
             // ErrorTextBox
             // 
-            this.ErrorTextBox.AcceptsReturn = true;
-            this.ErrorTextBox.AcceptsTab = true;
             this.ErrorTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.ErrorTextBox.Location = new System.Drawing.Point(12, 36);
-            this.ErrorTextBox.Multiline = true;
+            this.ErrorTextBox.Font = new System.Drawing.Font("Kozuka Gothic Pro R", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ErrorTextBox.Location = new System.Drawing.Point(15, 33);
             this.ErrorTextBox.Name = "ErrorTextBox";
-            this.ErrorTextBox.Size = new System.Drawing.Size(630, 329);
-            this.ErrorTextBox.TabIndex = 1;
+            this.ErrorTextBox.Size = new System.Drawing.Size(1047, 528);
+            this.ErrorTextBox.TabIndex = 2;
+            this.ErrorTextBox.Text = "";
+            // 
+            // SaveLogButton
+            // 
+            this.SaveLogButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveLogButton.Location = new System.Drawing.Point(972, 5);
+            this.SaveLogButton.Name = "SaveLogButton";
+            this.SaveLogButton.Size = new System.Drawing.Size(90, 23);
+            this.SaveLogButton.TabIndex = 3;
+            this.SaveLogButton.Text = "Save Log";
+            this.SaveLogButton.UseVisualStyleBackColor = true;
+            this.SaveLogButton.Click += new System.EventHandler(this.SaveLogButton_Click);
+            // 
+            // CopyToClipboardButton
+            // 
+            this.CopyToClipboardButton.Location = new System.Drawing.Point(860, 5);
+            this.CopyToClipboardButton.Name = "CopyToClipboardButton";
+            this.CopyToClipboardButton.Size = new System.Drawing.Size(106, 23);
+            this.CopyToClipboardButton.TabIndex = 4;
+            this.CopyToClipboardButton.Text = "Copy To Clipboard";
+            this.CopyToClipboardButton.UseVisualStyleBackColor = true;
+            this.CopyToClipboardButton.Click += new System.EventHandler(this.CopyToClipboardButton_Click);
             // 
             // ReportForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(654, 377);
+            this.ClientSize = new System.Drawing.Size(1074, 573);
+            this.Controls.Add(this.CopyToClipboardButton);
+            this.Controls.Add(this.SaveLogButton);
             this.Controls.Add(this.ErrorTextBox);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -74,7 +99,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox ErrorTextBox;
+        private System.Windows.Forms.RichTextBox ErrorTextBox;
+        private System.Windows.Forms.Button SaveLogButton;
+        private System.Windows.Forms.Button CopyToClipboardButton;
     }
 }
 
--- CodeWalker.ErrorReport/ReportForm.cs
+++ CodeWalker.ErrorReport/ReportForm.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace CodeWalker.ErrorReport
@@ -20,47 +15,97 @@ namespace CodeWalker.ErrorReport
 
         private void ReportForm_Load(object sender, EventArgs e)
         {
-            EventLog myLog = new EventLog();
-            myLog.Log = "Application";
-            //myLog.Source = ".NET Runtime";
+            ErrorTextBox.Clear();
 
-            var lastEntry = myLog.Entries[myLog.Entries.Count - 1];
-            var last_error_Message = lastEntry.Message;
-
+            EventLog appLog = new EventLog("Application");
             bool found = false;
 
-            for (int index = myLog.Entries.Count - 1; index > 0; index--)
+            for (int i = appLog.Entries.Count - 1; i >= 0; i--)
             {
-                var errLastEntry = myLog.Entries[index];
-                if (errLastEntry.EntryType == EventLogEntryType.Error)
+                EventLogEntry entry = appLog.Entries[i];
+                if (entry.EntryType == EventLogEntryType.Error && entry.Source == ".NET Runtime")
                 {
-                    if (errLastEntry.Source == ".NET Runtime")
+                    string message = entry.Message;
+                    string[] lines = message.Split(new[] { '\n' }, StringSplitOptions.None);
+
+                    if (lines.Length > 0 &&
+                        (lines[0].Contains("CodeWalker.exe") ||
+                         lines[0].Contains("CodeWalker RPF Explorer.exe") ||
+                         lines[0].Contains("CodeWalker Ped Viewer.exe") ||
+                         lines[0].Contains("CodeWalker Vehicle Viewer.exe")))
                     {
-                        var msg = errLastEntry.Message;
-                        var lines = msg.Split('\n');
-                        if (lines.Length > 0)
+                        found = true;
+
+                        AppendColoredText(ErrorTextBox, "Time: ", Color.DimGray);
+                        AppendColoredText(ErrorTextBox, entry.TimeGenerated.ToString() + "\r\n", Color.DimGray);
+
+                        AppendColoredText(ErrorTextBox, "=====================\r\n", Color.LightGray);
+
+                        foreach (var line in lines)
                         {
-                            var l = lines[0];
-                            if (l.Contains("CodeWalker.exe") ||
-                                l.Contains("CodeWalker RPF Explorer.exe") ||
-                                l.Contains("CodeWalker Ped Viewer.exe") ||
-                                l.Contains("CodeWalker Vehicle Viewer.exe"))
-                            {
-                                ErrorTextBox.Text = msg.Replace("\n", "\r\n");
-                                found = true;
-                                break;
-                            }
+                            AppendColoredText(ErrorTextBox, line.Trim() + "\r\n", Color.Black);
                         }
+
+                        break;
                     }
                 }
             }
 
             if (!found)
             {
-                ErrorTextBox.Text = "Event Log entry not found!";
-                MessageBox.Show("Unable to find the last CodeWalker.exe error in the Event Log.");
+                AppendColoredText(ErrorTextBox, "Event Log entry not found!\r\n", Color.Gray);
+                MessageBox.Show("Unable to find the last CodeWalker error in the Event Log.");
             }
-
         }
+
+        private void AppendColoredText(RichTextBox box, string text, Color color)
+        {
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+
+            box.SelectionColor = color;
+            box.AppendText(text);
+            box.SelectionColor = box.ForeColor; // Reset to default color
+        }
+
+        private void SaveLogButton_Click(object sender, EventArgs e)
+        {
+            using (var saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Title = "Save Error Log";
+                saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+                saveFileDialog.FileName = "log.txt";
+                saveFileDialog.DefaultExt = "txt";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+
+                    try
+                    {
+                        File.WriteAllText(filePath, ErrorTextBox.Text);
+                        MessageBox.Show($"Log saved successfully to:\n{filePath}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Failed to save the log file:\n{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void CopyToClipboardButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(ErrorTextBox.Text))
+            {
+                string wrappedText = $"```\r\n{ErrorTextBox.Text}\r\n```";
+                Clipboard.SetText(wrappedText);
+            }
+            else
+            {
+                MessageBox.Show("There is no text to copy.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
     }
 }
